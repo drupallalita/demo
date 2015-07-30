c35673e074ff3f5dbfa18409ce3bd9c2743f8fbd
Lalitha <lalithaa@techaspect.com>
2015-07-30 18:00:05 +0530

    building UI for the page

--- sites/all/modules/custom/question/question-list.tpl.php
+++ sites/all/modules/custom/question/question-list.tpl.php
@@ -2,23 +2,16 @@
 
 /**
  * @file
- * Default theme implementation for wrapping member listings and their
- * profiles.
+ * Default theme implementation for wrapping question listings and their
+ * counts.
  *
- * This template is used when viewing a list of users. It can be a general
- * list for viewing all users with the URL of "example.com/profile" or when
- * viewing a set of users who share a specific value for a profile such
- * as "example.com/profile/country/belgium".
+ * This template is used when viewing a list of questions with count of Votes, Answers and Views. It can be a general
+ * list for viewing all questions with the URL of "example.com/question" or in Home Page.
  *
  * Available variables:
- * - $content: User account profiles iterated through profile-listing.tpl.php.
- * - $current_field: The named field being browsed. Provided here for context.
- *   The above example would result in "last_name". An alternate template name
- *   is also based on this, e.g., "profile-wrapper-last_name.tpl.php".
+ * - $content: All nodes related to 'Question' Content type iterated through question-list.tpl.php.
  *
- * @see template_preprocess_profile_wrapper()
+ * @see template_preprocess_question_list()
  */
 ?>
-<?php // dsm($content); ?>
 <?php print $content; ?>
-
--- sites/all/modules/custom/question/question-row.tpl.php
+++ sites/all/modules/custom/question/question-row.tpl.php
@@ -1,36 +1,47 @@
 <?php
 /**
  * @file
- * Default theme implementation for displaying a user and their profile data
- * for member listing pages.
+ * Custom theme implementation for displaying questions listing pages.
  *
- * @see profile-wrapper.tpl.php
+ * @see question-row.tpl.php
  *      where all the data is collected and printed out.
  *
  * Available variables:
- * - $fields: User's account name linking to the users page.
- * - $questions: Keyed array of all profile fields that are set as visible
- *   in member list pages (configured by site administrators). It also needs
- *   to have a value in order to be present.
+ * - $fields: All nodes related to 'Question' Content type iterated through question-row.tpl.php.
+ * - $questions: Each nodes related to 'Question' Content type
  *
- * Each $field in $question contains:
- * - $field->title: Title of the profile field.
- * - $field->value: Value of the profile field.
- * - $field->type: Type of the profile field, i.e., checkbox, textfield,
- *   textarea, selection, list, url or date.
+ * Since $fields is keyed, a direct print of the field is not possible. 
+ * So each $value in $fields contains: Count of Votes, Answers and Views and Question Summary
  *
- * Since $profile is keyed, a direct print of the field is possible. Not
- * all accounts may have a value for a profile so do a check first. 
- *
- * @see template_preprocess_question_list()
+ * @see template_preprocess_question_row()
  */
 ?>
-<div id="question">
-  <?php if(isset($fields)): ?>
-    <?php foreach ($fields as $key=>$value): ?>
-      <div class='even'>
-        <?php print $value['votes']." - ".$value['answers']." - ".$value['views']." - ".$value['qns_desc']; ?>
+  
+<?php if(isset($fields)): ?>
+  <?php foreach ($fields as $key=>$value): ?>
+    <div class="question-summary narrow">
+      <div class="cp">
+        <div class="votes">
+          <!-- Question : No. of Votes  -->
+          <div class="mini-counts"><span><?php print $value['votes']; ?></span></div>
+          <div>votes</div>
+        </div>
+        <div class="answers">
+          <!-- Question : No. of Answers  -->
+          <div class="mini-counts"><span><?php print $value['answers']; ?></span></div>
+          <div>answers</div>
+        </div>
+        <div class="views">
+          <!-- Question : No. of Views  -->
+          <div class="mini-counts"><span><?php print $value['views']; ?></span></div>
+          <div>views</div>
+        </div>
       </div>
-    <?php endforeach; ?>
-  <?php endif; ?>
-</div>
+      <div class="summary">
+        <!-- Question Description / Summary -->
+        <h3><a href="#" class="question-hyperlink" title="sample title"><?php print $value['qns_desc']; ?></a></h3>
+      </div>
+    </div>
+  <?php endforeach; ?>
+<?php endif; ?>
+  
